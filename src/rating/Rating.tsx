aa21da1fe0893809deb19ef428fac6477b2c9612
e1ing <elvira.kisling@gmail.com>
2021-04-21 22:37:01 +0300

    video-08.1

--- src/rating/Rating.tsx
+++ src/rating/Rating.tsx
@@ -9,11 +9,11 @@ let[value, setValue] = useState(4)
 
     return (
         <div>
-            <Star selected={value>0}/> <button onClick={() => {setValue(1)}}>1</button>
-            <Star selected={value>2}/> <button onClick={() => {setValue(2)}}>2</button>
-            <Star selected={value>3}/> <button onClick={() => {setValue(3)}}>3</button>
-            <Star selected={value>4}/> <button onClick={() => {setValue(4)}}>4</button>
-            <Star selected={value>5}/> <button onClick={() => {setValue(5)}}>5</button>
+            <Star selected={value>1} setValue={setValue}/>
+            <Star selected={value>2} setValue={setValue}/>
+            <Star selected={value>3} setValue={setValue}/>
+            <Star selected={value>4} setValue={setValue}/>
+            <Star selected={value>5} setValue={setValue}/>
         </div>
     );
 }
@@ -21,13 +21,9 @@ let[value, setValue] = useState(4)
 
 type StarPropsType = {
     selected: boolean
+    setValue: (value: number) =>void
 }
 
 function Star(props: StarPropsType) {
-    if (props.selected === true) {
-        return <div><b>star</b></div>
-    } else {
-        return <div>star</div>
-
+   return <span onClick={() => {props.setValue(5)}}>  {props.selected ? <b>star</b> : "star"} </span>
     }
-}
