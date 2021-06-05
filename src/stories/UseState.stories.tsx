3810c3be1f1dd4a7c0456fbd2c8ddc3021cab63d
e1ing <elvira.kisling@gmail.com>
2021-06-05 22:04:28 +0300

    video-19

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,23 @@
+import React, {useMemo, useState} from 'react';
+
+export default {
+    title: "useState demo",
+}
+
+function generateData(){
+    console.log("generateData")
+    return 354545435;
+}
+
+export const Example1 = () => {
+    console.log("Example1")
+
+    const initValue = useMemo(generateData,[])
+
+    const [counter, setCounter] = useState(generateData)
+    
+    return <>
+        <button onClick={() => setCounter( (state: number) => state+1)}> +</button>
+        {counter}
+    </>
+}
